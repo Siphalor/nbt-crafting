d591d3f77354992b6a57803a4c4fca4087ac445d
Siphalor <info@siphalor.de>
2020-01-31 22:39:45 +0100

    Alpha 7 of Version 2.0.0
    - Remove debug dumps

--- src/main/java/de/siphalor/nbtcrafting/mixin/network/MixinSynchronizeRecipesS2CPacket.java
+++ src/main/java/de/siphalor/nbtcrafting/mixin/network/MixinSynchronizeRecipesS2CPacket.java
@@ -3,7 +3,6 @@ package de.siphalor.nbtcrafting.mixin.network;
 import de.siphalor.nbtcrafting.NbtCrafting;
 import de.siphalor.nbtcrafting.util.ServerRecipe;
 import de.siphalor.nbtcrafting.util.duck.IServerPlayerEntity;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.network.packet.SynchronizeRecipesS2CPacket;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.util.PacketByteBuf;
@@ -13,11 +12,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,20 +29,6 @@ public abstract class MixinSynchronizeRecipesS2CPacket {
 			buf.writeVarInt(syncRecipes.size());
 			syncRecipes.forEach(recipe -> writeRecipe(recipe, buf));
 			callbackInfo.cancel();
-		}
-	}
-
-	@Inject(method = "read", at = @At("HEAD"))
-	public void onRead(PacketByteBuf buf, CallbackInfo callbackInfo) {
-		File dir = FabricLoader.getInstance().getGameDirectory().toPath().resolve("nbtc-debug").toFile();
-		dir.mkdirs();
-		File file = dir.toPath().resolve(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss-S").format(LocalDateTime.now()) + ".dump").toFile();
-		try {
-			FileOutputStream fileOutputStream = new FileOutputStream(file);
-			NbtCrafting.tempBB.forEachByte(value -> {fileOutputStream.write(value); return true;});
-			fileOutputStream.close();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 	}
 }
