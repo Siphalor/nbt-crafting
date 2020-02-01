ce33fed679c46f7af2109ce9e46a605ebc4ede1c
Siphalor <info@siphalor.de>
2020-02-01 15:32:37 +0100

    Alpha 10 of Version 2.0.0
    * Fix #29 (REI recipe filling not working)

---
+++ src/main/java/de/siphalor/nbtcrafting/rei/mixin/MixinREIRecipeFinder.java
@@ -0,0 +1,27 @@
+package de.siphalor.nbtcrafting.rei.mixin;
+
+import me.shedaniel.rei.server.RecipeFinder;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(RecipeFinder.class)
+public class MixinREIRecipeFinder {
+	/**
+	 * @author Siphalor
+	 * @reason Fix copied code in REI
+	 */
+	@Overwrite
+	public static int getItemId(ItemStack itemStack) {
+		return net.minecraft.recipe.RecipeFinder.getItemId(itemStack);
+	}
+
+	/**
+	 * @author Siphalor
+	 * @reason Fix copied code in REI
+	 */
+	@Overwrite
+	public static ItemStack getStackFromId(int itemId) {
+		return net.minecraft.recipe.RecipeFinder.getStackFromId(itemId);
+	}
+}
