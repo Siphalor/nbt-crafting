d4a9e1cd7cc8c47cea43c1d7371789a381551238
Siphalor <info@siphalor.de>
2021-01-06 13:30:33 +0100

    Fix #61 (Mixin reports errors when REI is not present)

---
+++ src/main/java/de/siphalor/nbtcrafting/compat/NbtCCompatMixinConfig.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2020 Siphalor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package de.siphalor.nbtcrafting.compat;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class NbtCCompatMixinConfig implements IMixinConfigPlugin {
+	@Override
+	public void onLoad(String mixinPackage) {
+
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		switch (mixinClassName) {
+			case "de.siphalor.nbtcrafting.compat.mixin.MixinRegistrySyncManager":
+				return FabricLoader.getInstance().isModLoaded("fabric-networking-v0");
+			case "de.siphalor.nbtcrafting.compat.mixin.MixinREIRecipeFinder":
+				return FabricLoader.getInstance().isModLoaded("roughlyenoughitems");
+		}
+		return false;
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+	}
+
+	@Override
+	public List<String> getMixins() {
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+}
