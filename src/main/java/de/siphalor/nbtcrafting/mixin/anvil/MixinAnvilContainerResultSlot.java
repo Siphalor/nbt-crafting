12d177665379d72648d14f2d1072970902c830a1
Siphalor <info@siphalor.de>
2021-08-15 18:01:20 +0200

    Fix anvils consuming whole base stacks

--- src/main/java/de/siphalor/nbtcrafting/mixin/anvil/MixinAnvilContainerResultSlot.java
+++ src/main/java/de/siphalor/nbtcrafting/mixin/anvil/MixinAnvilContainerResultSlot.java
@@ -10,6 +10,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -19,11 +20,18 @@ public abstract class MixinAnvilContainerResultSlot extends Slot {
 	@Shadow(aliases = "field_7781")
 	private AnvilContainer container;
 
+	@Unique
+	private ItemStack originalBaseStack;
+
 	public MixinAnvilContainerResultSlot(Inventory inventory, int invSlot, int xPosition, int yPosition) {
 		super(inventory, invSlot, xPosition, yPosition);
 	}
 
-	@Inject(method = "canTakeItems", at = @At("HEAD"), cancellable = true)
+	@Inject(
+			method = "canTakeItems(Lnet/minecraft/entity/player/PlayerEntity;)Z",
+			at = @At("HEAD"),
+			cancellable = true
+	)
 	public void canTakeItemsTop(PlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
 		if (((AnvilContainerAccessor) container).getLevelCost().get() <= 0) {
 			ItemStack base = container.getSlot(0).getStack();
@@ -32,9 +40,26 @@ public abstract class MixinAnvilContainerResultSlot extends Slot {
 			}
 		}
 	}
+
+	@Inject(
+			method = "onTakeItem(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;)" +
+					"Lnet/minecraft/item/ItemStack;",
+			at = @At("HEAD")
+	)
+	public void onTakeItemTop(CallbackInfoReturnable<ItemStack> cir) {
+		originalBaseStack = container.getSlot(0).getStack();
+	}
 	
-	@Inject(method = "onTakeItem", at = @At("RETURN"))
+	@Inject(
+			method = "onTakeItem(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;)" +
+					"Lnet/minecraft/item/ItemStack;",
+			at = @At("RETURN")
+	)
 	public void onItemTaken(PlayerEntity player, ItemStack stack, CallbackInfoReturnable<ItemStack> cir) {
+		if (originalBaseStack != null) {
+			originalBaseStack.decrement(1);
+			container.setStackInSlot(0, originalBaseStack);
+		}
 		if (player instanceof ServerPlayerEntity) {
 			if (!NbtCrafting.hasClientMod((ServerPlayerEntity) player)) {
 				((ServerPlayerEntity) player).networkHandler.sendPacket(new ContainerSlotUpdateS2CPacket(
