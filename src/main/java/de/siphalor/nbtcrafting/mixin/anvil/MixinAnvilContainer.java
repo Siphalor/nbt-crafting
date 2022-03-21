a052558e40d586716f1adea36559596e21b6482d
Siphalor <info@siphalor.de>
2022-03-21 13:21:14 +0100

    Revert "Anvil recipes support remainders on base input."
    This reverts commit b191806b69861aaaa70fe5a7661ee339bc0e7ab9.

--- src/main/java/de/siphalor/nbtcrafting/mixin/anvil/MixinAnvilContainer.java
+++ src/main/java/de/siphalor/nbtcrafting/mixin/anvil/MixinAnvilContainer.java
@@ -17,21 +17,16 @@
 
 package de.siphalor.nbtcrafting.mixin.anvil;
 
-import de.siphalor.nbtcrafting.api.RecipeUtil;
-import de.siphalor.nbtcrafting.recipe.IngredientRecipe;
-
 import io.netty.buffer.Unpooled;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.s2c.play.ScreenHandlerSlotUpdateS2CPacket;
 import net.minecraft.screen.*;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
-import net.minecraft.util.collection.DefaultedList;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
@@ -45,8 +40,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import de.siphalor.nbtcrafting.NbtCrafting;
 import de.siphalor.nbtcrafting.recipe.AnvilRecipe;
-
-import java.util.Optional;
 
 @Mixin(AnvilScreenHandler.class)
 public abstract class MixinAnvilContainer extends ForgingScreenHandler {
@@ -68,9 +61,6 @@ public abstract class MixinAnvilContainer extends ForgingScreenHandler {
 
 	@Unique
 	private AnvilRecipe recipe = null;
-
-	@Unique
-	private static DefaultedList<ItemStack> remainders = null;
 
 	public MixinAnvilContainer(@Nullable ScreenHandlerType<?> type, int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
 		super(type, syncId, playerInventory, context);
@@ -135,9 +125,6 @@ public abstract class MixinAnvilContainer extends ForgingScreenHandler {
 	)
 	public void onTakeItemTop(PlayerEntity player, ItemStack stack, CallbackInfo ci) {
 		originalBaseStack = getSlot(0).getStack();
-
-		Optional<AnvilRecipe> match = player.world.getRecipeManager().getFirstMatch(NbtCrafting.ANVIL_RECIPE_TYPE, input, player.world);
-		remainders = match.map(anvilRecipe -> anvilRecipe.getRemainder(input)).orElse(null);
 	}
 
 	@Inject(
@@ -152,13 +139,6 @@ public abstract class MixinAnvilContainer extends ForgingScreenHandler {
 				stack.onCraft(player.world, player, stack.getCount());
 			}
 		}
-
-		if (remainders != null && !remainders.isEmpty()) {
-			context.run(((world, pos) -> {
-				RecipeUtil.putRemainders(remainders, input, world, pos);
-			}));
-		}
-
 		if (player instanceof ServerPlayerEntity) {
 			if (!NbtCrafting.hasClientMod((ServerPlayerEntity) player)) {
 				((ServerPlayerEntity) player).networkHandler.sendPacket(new ScreenHandlerSlotUpdateS2CPacket(
