bdae77f828340a28eeb8ca59f8f55a006c7a77b3
Siphalor <info@siphalor.de>
2022-09-17 21:26:26 +0200

    Reapply: Fix dirty crafting inventory not propagating to recipe matches

---
+++ src/main/java/de/siphalor/nbtcrafting3/mixin/crafting/MixingCraftingInventory.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020-2022 Siphalor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package de.siphalor.nbtcrafting3.mixin.crafting;
+
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.screen.ScreenHandler;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CraftingInventory.class)
+public class MixingCraftingInventory {
+	@Shadow
+	@Final
+	private ScreenHandler handler;
+
+	@Inject(method = "markDirty", at = @At("HEAD"))
+	public void onMarkDirty(CallbackInfo callbackInfo) {
+		handler.onContentChanged((CraftingInventory) (Object) this);
+	}
+}
